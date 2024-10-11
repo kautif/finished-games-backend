f911d16c0e9f36f44e37cbb618974d7af4784f55
ehtisham91 <ehtishamashraf91@gmail.com>
2024-10-11 14:27:42 +0500

    Updated token expiration time for testing

--- services/generateTokens.js
+++ services/generateTokens.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 
 // Function to generate a new access token
 const generateAuthToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" }); // Shorter expiration for access token
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30m" }); // Shorter expiration for access token
 };
 
 // Function to generate a new refresh token
 const generateRefreshToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "1h",
   }); // Longer expiration for refresh token
 };
 
